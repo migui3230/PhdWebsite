c6b7998621c64d49828c4f2276bc520925f02866
Miguel Sarenas <migui0323@gmail.com>
2022-12-23 15:15:26 -0500

    add dummy link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,15 @@ const Layout = ({ children }) => {
   return (
     <div>
       {/* render the navbar component with a dummy link */}
-      <Navbar links={[{ link: "#", label: "Link", links: [] }]} />
+      <Navbar
+        links={[
+          {
+            link: "#",
+            label: "Link",
+            links: [{ link: "https://www.google.com/", label: "google" }],
+          },
+        ]}
+      />
       <div>{children}</div>
     </div>
   );
